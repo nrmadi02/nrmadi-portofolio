64a44b25a7c306eb3ef58a708e295a2da8b5b0d9
Nur Ahmadi <ahmadinur2202@gmail.com>
2023-02-23 00:10:26 +0800

    update initial

--- src/components/SEO/HeadSeo.tsx
+++ src/components/SEO/HeadSeo.tsx
@@ -22,19 +22,15 @@ const HeadSeo = ({
 }: SeoProps) => {
   return (
     <Head>
-      //basic metadata
       <title>{title}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content={description} />
-      //twitter metadata
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content={siteMetadata.twitterHandle} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={ogTwitterImage} />
-      //canonical link
       <link rel="canonical" href={canonicalUrl} />
-      //open graph metadata
       <meta property="og:locale" content="en_US" />
       <meta property="og:site_name" content={siteMetadata.companyName} />
       <meta property="og:type" content={ogType} />
